22bb0490db8a9ec13d730996ca1b8a0588c8370c
Jesse <weihongji@gmail.com>
2013-08-04 22:04:44 +0800

    Add to github

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+		static void Main(string[] args) {
+			log.Fatal("Fatal");
+			log.Error("Error");
+			log.Warn("Warn");
+			log.Info("Info");
+			log.Debug("Debug");
+
+			Console.WriteLine("Press any key to exit ...");
+			Console.ReadKey();
+		}
+	}
+}
