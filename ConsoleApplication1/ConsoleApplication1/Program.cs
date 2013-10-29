24ba5aec4580400fe3e01f8e839b657afdb6bd85
Jesse <weihongji@gmail.com>
2013-10-29 15:47:02 +0800

    Moved the [assembly:...] line into Program.cs from AssemblyInfo.cs.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,38 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using log4net;
 
 using System.Timers;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace ConsoleApplication1
 {
-	class Program
-	{
-		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
-		private static int counter = 0;
-		private static Timer timer;
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static int counter = 0;
+        private static Timer timer;
 
-		static void Main(string[] args) {
-			timer = new Timer(1000 * 60);
-			timer.Enabled = true;
-			timer.Elapsed += timer_Elapsed;
+        static void Main(string[] args) {
+            //timer = new Timer(1000 * 60);
+            //timer.Enabled = true;
+            //timer.Elapsed += timer_Elapsed;
 
-			log.Fatal("Fatal");
-			log.Error("Error");
-			log.Warn("Warn");
-			log.Info("Info");
-			log.Debug("Debug");
+            log.Fatal("Fatal");
+            log.Error("Error");
+            log.Warn("Warn");
+            log.Info("Info");
+            log.Debug("Debug");
 
-			System.Threading.Thread.Sleep(1000 * 60 * 3);
+            //System.Threading.Thread.Sleep(1000 * 60 * 3);
 
-			Console.WriteLine("Press any key to exit ...");
-			Console.ReadKey();
-		}
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
 
-		static void timer_Elapsed(object sender, ElapsedEventArgs e) {
-			counter++;
-			log.Info(counter.ToString());
-		}
-	}
+        static void timer_Elapsed(object sender, ElapsedEventArgs e) {
+            counter++;
+            log.Info(counter.ToString());
+        }
+    }
 }
