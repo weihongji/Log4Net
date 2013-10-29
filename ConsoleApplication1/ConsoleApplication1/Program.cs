cd9f9a25032d5d881e8851e505d8b8fbe31dd666
Jesse <weihongji@gmail.com>
2013-10-29 09:34:52 +0800

    Log file roller.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,21 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
+using System.Timers;
+
 namespace ConsoleApplication1
 {
 	class Program
 	{
 		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+		private static int counter = 0;
+		private static Timer timer;
 
 		static void Main(string[] args) {
+			timer = new Timer(1000 * 60);
+			timer.Enabled = true;
+			timer.Elapsed += timer_Elapsed;
+
 			log.Fatal("Fatal");
 			log.Error("Error");
 			log.Warn("Warn");
 			log.Info("Info");
 			log.Debug("Debug");
 
+			System.Threading.Thread.Sleep(1000 * 60 * 3);
+
 			Console.WriteLine("Press any key to exit ...");
 			Console.ReadKey();
+		}
+
+		static void timer_Elapsed(object sender, ElapsedEventArgs e) {
+			counter++;
+			log.Info(counter.ToString());
 		}
 	}
 }
